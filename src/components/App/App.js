43d12f917df14a0318476e27cfbbef0fdf84f8fd
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-11 14:20:50 -0400

    Delete History component, create refresher lib

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import LogoutForm from '../LogoutForm/LogoutForm';
-import History from '../History/History';
+import refresher from '../../refresher';
 
 import './App.css';
 
@@ -11,59 +11,39 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      token    : null,
-      history : [],
+      loggedIn: false,
     };
 
     this.handleLogin  = this.handleLogin.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
   }
 
+  componentDidMount() {
+    this.refresher = refresher.createRefresher(window.localStorage, 'token');
+  }
+
   handleLogin(token) {
-
-    const action = {
-      type: 'login',
-      data: token,
-      time: new Date(Date.now()),
-    };
-
-    this.setState({
-      token: token,
-      history: [...this.state.history, action],
-    });
+    this.setState({ loggedIn: true });
+    window.localStorage.setItem('token', token);
+    this.refresher.start();
   }
 
   handleLogout() {
-
-    const action = {
-      type: 'logout',
-      data: null,
-      time: new Date(Date.now()),
-    };
-
-    this.setState({
-      token: null,
-      history: [...this.state.history, action],
-    });
+    this.setState({ loggedIn: false });
+    window.localStorage.removeItem('token');
+    this.refresher.stop();
   }
 
   render() {
-
-    let form;
-
-    if (this.state.token) {
-      form = <LogoutForm onLogout={this.handleLogout} />;
-    } else {
-      form = <LoginForm onLogin={this.handleLogin} />;
-    }
 
     return (
       <div id="app" className="container-fluid">
         <h1>JWT Fresh</h1>
 
-        {form}
+        { !this.state.loggedIn && <LoginForm onLogin={this.handleLogin} />    }
 
-        <History data={this.state.history} />
+        { this.state.loggedIn  && <LogoutForm onLogout={this.handleLogout} /> }
+
       </div>
     );
   }
